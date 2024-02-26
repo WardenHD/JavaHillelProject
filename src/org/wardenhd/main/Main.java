1e3f64e1c116ec90e16460404ac53c12dd0a8ca6
WardenHD <tomas.bezruchko@gmail.com>
2024-02-26 20:48:44 +0200

    Upload project entry

---
+++ src/org/wardenhd/main/Main.java
@@ -0,0 +1,11 @@
+package org.wardenhd.main;
+
+import javax.swing.*;
+
+public class Main {
+    JFrame frame = new JFrame("Window App");
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/org/wardenhd/window/WindowManager.java
@@ -0,0 +1,16 @@
+package org.wardenhd.window;
+
+import javax.swing.*;
+
+public class WindowManager {
+    JFrame frame;
+
+    public static void init(JFrame frame) {
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+    }
+
+    private void setupComponents() {
+
+    }
+}
